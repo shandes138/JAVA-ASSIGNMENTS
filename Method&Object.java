054f82cf228c0e0c97d5b55f0d728926d976241f
shandes <145603607+shandes138@users.noreply.github.com>
2024-02-03 00:25:34 +0530

    Create Method&Object.java
    Solution of the method and object assignment

---
+++ Method&Object.java
@@ -0,0 +1,62 @@
+QUESTION 1:- How to create an object in java`?
+ANSWER :- There are many ways by which we can create objects of a class in java.In Java a class provides
+the blueprint for objects.
+ 
+Different ways to create objects in Java Are:-  
+1. Using new keyword
+2. Using new instance
+3. Using clone() method
+4. Using  deserialization;
+5. Using newInstance()  method of constructor class 
+
+
+
+QUESTION 2:- What is the use of a new keyword in Java`?
+ANSWER:- The "new" keyword in Java is used to create an instance of an object. It allocates memory to an object and
+returns a reference to the object created. It is used with a constructor to create an object.
+
+
+
+        
+QUESTION 3:- What are the different types of variables in Java?
+ANSWER:- There are three types of variables in java
+a.Static Variable
+b.Instance Variable
+c.Local Variable 
+
+
+
+QUESTION 4:- 4. What is the difference between Instance variables and Local variables?
+ANSWER:-The difference between instance variable and local variable are as followes:-
+A.LOCAL VARIABLE :-
+        --> It will be created at the time of object creation and destroyed at the time of object destruction and hence the scope of instace
+        is exactly the sae as scope of object.
+        --> It is stored in the heap as part of object.
+        --> Should be declared within the class directlt but outside of any method or block or constructor.
+
+B.LOCAL VARIABLE:-
+        -->They are stored in the stack 
+        --> They are created as part of block execution in which it is declared and destroyed once that block execution completes. Hence the scope
+         of local variable is exactly the same as the scope of the block in which we declared. 
+
+
+
+QUESTION 5:- In which area memory is allocated for instance variable and local variable?
+ANSWER:- Instance variables are allocated in the heap and local variables are allocated in the stack.
+
+
+
+QUESTION 6:- What is method overloading?
+ANSWER:- Method overloading in Java is a feature that allows a class to have multiple methods with the same name
+but different parameters. The Java compiler distinguishes these methods by the number, type, and order of
+parameters. Overloading is used to provide multiple ways to call a method for different use cases, making
+code more readable and reusable.
+        
+
+
+
+
+        
+        
+
+ 
